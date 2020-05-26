baf260b4b46b2559265ec07adb2bde9f0e4e5e65
BalintKoczor <balint.koczor@materials.ox.ac.uk>
2020-05-26 13:57:18 +0100

    refactored build script

--- build.sh
+++ build.sh
@@ -3,27 +3,6 @@ if ! [ -x "$(command -v gcc)" ]; then
   echo 'Error: gcc is not installed.' >&2
   exit 1
 fi
-if ! [ -x "$(command -v tar)" ]; then
-  echo 'Error: tar is not installed.' >&2
-  exit 1
-fi
-if [ ! -d "./Calculated" ]; then
-  mkdir Calculated
-  cd Calculated
-  echo "===================================="
-  echo "Exporting precalculated eigenvectors"
-  echo "===================================="
-  tar -xzvf ../Eigenvectors.tar.gz
-  echo "========================================"
-  echo "Exporting precalculated parity operators"
-  echo "========================================"
-  tar -xzvf ../Parity.tar.gz
-  echo "======================================="
-  echo "Exporting precalculated example Kernels"
-  echo "======================================="
-  tar -xzvf ../Kernels.tar.gz
-  cd ..
-fi
 if [ ! -d "./Executables" ]; then
 	mkdir Executables
 fi
@@ -31,7 +10,7 @@ cd Executables
 echo "==========================================="
 echo "Compiling sourcecode: Precalculate_Kernel.c"
 echo "==========================================="
-gcc -O3 -std=c99 ../src/Precalculate_Kernel.c -o ./Precalculate_Kernel.a
+gcc -Ofast -march=native -std=c99 ../src/Precalculate_Kernel.c -o ./Precalculate_Kernel.a
 echo "======================"
 echo "Precalculating kernels"
 echo "======================"
@@ -39,7 +18,7 @@ echo "======================"
 echo "===================================================="
 echo "Compiling sourcecode example: EfficientCalculation.c"
 echo "===================================================="
-gcc -O3 -std=c99 ../src/EfficientCalculation.c -o ./EfficientCalculation.a
+gcc -Ofast -march=native -std=c99 ../src/EfficientCalculation.c -o ./EfficientCalculation.a
 echo "================"
 echo "Runnning example"
 echo "================"
