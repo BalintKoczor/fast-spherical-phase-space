8258b6667f79a8ef63c3977ca256b06a00d686b3
BalintKoczor <balint.koczor@materials.ox.ac.uk>
2020-06-11 11:29:07 +0100

    added python module

---
+++ Python/PhaseSpaceRepresentations.py
@@ -0,0 +1,63 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def precalculatedKcoeffs(Ndim, path2kernels):
+    fftdim = 2*Ndim-1;
+    kernel = np.fromfile(path2kernels+'KernelD'+str(Ndim)+'.dat', dtype=np.complex128)
+    kernel = (np.flip(kernel)).reshape((fftdim,Ndim,Ndim))
+    return kernel
+    
+def PSrepresentationFourierCoeff(rho, Kcoeffs):
+    Ndim = len(rho)
+    fftdim = 2*Ndim-1;
+    result = np.zeros((fftdim,fftdim),dtype=np.complex128)
+    #This is an implementation using the offset trace operation
+    #of the element-wise product of matrices
+    for l in range(0, fftdim):
+        for m in range(0, fftdim):
+            prod = np.multiply(rho, Kcoeffs[l])
+            result[l,m] = np.trace(prod,m-Ndim+1)
+    return result
+
+
+def PSrepresentationFourierCoeff_LOOP(rho, Kcoeffs):
+    Ndim = len(rho)
+    fftdim = 2*Ndim-1;
+    result = np.zeros((fftdim,fftdim),dtype=np.complex128)
+    #This is straightforwad loop-based implementation which might be slow
+    for l in range(0, fftdim):
+        for m in range(0, fftdim):
+            pres = 0
+            lowb = max(Ndim - m - 1, 0)
+            upb  = min(2*Ndim - m -1, Ndim)
+            for lam in range(lowb, upb):
+                pres = pres + rho[lam,lam+m-Ndim+1]*Kcoeffs[l,lam,lam+m-Ndim+1]
+            result[l,m] = pres
+    return result
+    
+def PSrepresentationFromFourier(rho, Kcoeffs, finalpoints):
+    Ndim = len(rho)
+    fourierCoeffs = PSrepresentationFourierCoeff(rho, Kcoeffs)
+    fourierCoeffsRef = PSrepresentationFourierCoeff(np.identity(Ndim), Kcoeffs)
+
+    PSrep = np.fft.ifft2(fourierCoeffs,(2*finalpoints,finalpoints))
+    PSrepRef = np.fft.ifft2(fourierCoeffsRef,(2*finalpoints,finalpoints))
+    Pi=np.pi
+    identityPS=identityPS=np.sqrt(((Ndim-1)/2)/(2*Pi))*np.sqrt(4*Pi)/np.sqrt(Ndim)
+    result = np.real((PSrep/PSrepRef*identityPS).round(12))
+    result = result[0:finalpoints]
+    return result
+    
+def PSrepPlot(psFunctionArray, filename):
+    ny, nx = psFunctionArray.shape
+
+    x = np.linspace(0, 2*np.pi, nx)
+    y = np.linspace(0, np.pi, ny)
+
+    xv, yv = np.meshgrid(x, y)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.view_init(60, 30)
+    ax.plot_surface(xv, yv, psFunctionArray, cmap='inferno', rstride=1, cstride=1, alpha=None, antialiased=True)
+    plt.savefig(filename)
---
+++ Python/example.py
@@ -0,0 +1,35 @@
+import PhaseSpaceRepresentations as ps
+import numpy as np
+
+# import and show the precalculated K matrices from
+#../Calculated/Kernel -- these correspond to the Wigner function
+Ndim=2 #set the dimension Ndim=2J+1 of the system
+path2kernels = '../Calculated/Kernels/'
+Kcoeffs = ps.precalculatedKcoeffs(Ndim, path2kernels)
+print('Example of precalculated transofmation matrices\nfor dimession', Ndim)
+for l in range(0,2*Ndim-1):
+    print('l = ', l-Ndim+1,'\n', Kcoeffs[l],'\n')
+    
+# set the dimension dim=2J+1 of the system *)
+Ndim=10
+
+# import the precalculated K coefficients from
+#../Calculated/Kernel -- these correspond to the Wigner function
+Kcoeffs = ps.precalculatedKcoeffs(Ndim, path2kernels)
+
+#Set the number of points along THETA and PHI after the Fourier 
+#transformation -- increasing the number of points results in a smoother plot 
+finalpoints=256
+
+#Prepare the deisred density matrix -- this is a Schroedinger cat state now 
+psi = np.zeros(Ndim)
+psi[0] = 1/np.sqrt(2)
+psi[Ndim-1] = 1/np.sqrt(2)
+rho = np.outer(psi,np.conjugate(psi))
+
+#Calculate the Wigner function of the density matrix
+result = ps.PSrepresentationFromFourier(rho, Kcoeffs, finalpoints)
+#plot the Wigner function and save to file
+filename = 'plot_D'+str(Ndim)+'.png'
+ps.PSrepPlot(result, filename)
+print('phase-space plot saved to file: ' + filename)
