b66f749df87a2cc8075d628258cf91a4b87cb705
BalintKoczor <balint.koczor@materials.ox.ac.uk>
2020-08-10 18:48:14 +0100

    Update build.bat
    minor fix

--- build.bat
+++ build.bat
@@ -12,19 +12,19 @@ echo.
 
 set PATH=%MinGW_DIR%\bin;%PATH%
 cd Executables
-echo "==========================================="
-echo "Compiling sourcecode: Precalculate_Kernel.c"
-echo "==========================================="
+echo ===========================================
+echo Compiling sourcecode: Precalculate_Kernel.c
+echo ===========================================
 gcc -Ofast -march=native -std=c99 ../src/Precalculate_Kernel.c -o ./Precalculate_Kernel.exe
-echo "======================"
-echo "Precalculating kernels"
-echo "======================"
+echo ======================
+echo Precalculating kernels
+echo ======================
 Precalculate_Kernel.exe
-echo "===================================================="
-echo "Compiling sourcecode example: EfficientCalculation.c"
-echo "===================================================="
+echo ====================================================
+echo Compiling sourcecode example: EfficientCalculation.c
+echo ====================================================
 gcc -Ofast -march=native -std=c99 ../src/EfficientCalculation.c -o ./EfficientCalculation.exe
-echo "================"
-echo "Runnning example"
-echo "================"
+echo ================
+echo Runnning example
+echo ================
 EfficientCalculation.exe
