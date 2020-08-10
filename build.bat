e29fdc3affc668d124529e62954526f5e649ec56
BalintKoczor <balint.koczor@materials.ox.ac.uk>
2020-08-10 18:45:02 +0100

    updated windows bat

--- build.bat
+++ build.bat
@@ -1,8 +1,30 @@
 @echo off
+cls
 set MinGW_DIR=C:\MinGW
 echo ---------------------------------------------------------------
 echo MinGW needs to be installed for running this program on Windows
-echo Assuming MinGW installation folder: %MinGW_DIR%
 echo ---------------------------------------------------------------
-set PATH=%MinGW_DIR%\msys\1.0\bin;%MinGW_DIR%\bin;%PATH%
-%MinGW_DIR%\msys\1.0\bin\bash.exe .\build.sh
+echo.
+echo ---------------------------------------------------------------
+echo Assuming MinGW installation folder: %MinGW_DIR% 
+echo ---------------------------------------------------------------
+echo.
+
+set PATH=%MinGW_DIR%\bin;%PATH%
+cd Executables
+echo "==========================================="
+echo "Compiling sourcecode: Precalculate_Kernel.c"
+echo "==========================================="
+gcc -Ofast -march=native -std=c99 ../src/Precalculate_Kernel.c -o ./Precalculate_Kernel.exe
+echo "======================"
+echo "Precalculating kernels"
+echo "======================"
+Precalculate_Kernel.exe
+echo "===================================================="
+echo "Compiling sourcecode example: EfficientCalculation.c"
+echo "===================================================="
+gcc -Ofast -march=native -std=c99 ../src/EfficientCalculation.c -o ./EfficientCalculation.exe
+echo "================"
+echo "Runnning example"
+echo "================"
+EfficientCalculation.exe
