a63fcd3769ce429a9a9180ca64a91b9ceee1b509
BalintKoczor <balint.koczor@materials.ox.ac.uk>
2021-02-01 15:17:36 +0000

    updated radius in Matlab module

--- Matlab/PSrepresentationFromFourier.m
+++ Matlab/PSrepresentationFromFourier.m
@@ -1,9 +1,10 @@
 function result = PSrepresentationFromFourier(rho, Kcoeffs, Ndim, finalpoints)
 psrep = PSrepresentationFourierCoeff(rho, Kcoeffs, Ndim);
-psrepREF = PSrepresentationFourierCoeff(eye(Ndim), Kcoeffs, Ndim);
+psrepREF = PSrepresentationFourierCoeff(eye(Ndim)/sqrt(Ndim), Kcoeffs, Ndim);
 result = fft2(psrep,2*finalpoints, finalpoints);
 resultREF = fft2(psrepREF,2*finalpoints, finalpoints);
-identityPS = sqrt(((Ndim-1)/2)/(2*pi))*sqrt(4*pi)/sqrt(Ndim);
-result = result ./ resultREF/identityPS;
+J=(Ndim-1)/2;
+identityPS = 1/sqrt(2*J);
+result = result ./ resultREF*identityPS;
 result = result(1:finalpoints,:);
 end
